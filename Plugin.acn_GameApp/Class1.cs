64e2378e856c196cf9d99f6b183c1b91a8a34fa5
Anatoli Scameroni <anatoli.scameroni@avanade.com>
2025-04-01 11:52:15 +0200

    Add project files.

---
+++ Plugin.acn_GameApp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.acn_GameApp
+{
+    public class Class1
+    {
+    }
+}
