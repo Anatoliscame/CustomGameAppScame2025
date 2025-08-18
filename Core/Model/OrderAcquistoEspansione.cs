c27b1f5849491f813a448903bbaef1e860b6f322
Anatoli Scameroni <anatoli.scameroni@avanade.com>
2025-08-18 13:50:37 +0200

    KECM-1001: Add solution "Core"

---
+++ Core/Model/OrderAcquistoEspansione.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class OrderAcquistoEspansione
+    {
+        public Guid OrderAcquistoEId { get; set; }
+        public string Name { get; set; }
+        public string KeyGameCode { get; set; }
+        public string NameContentVideoGame { get; set; }
+        public OrdineAcquisto OrdineAcquisto { get; set; }
+    }
+}
