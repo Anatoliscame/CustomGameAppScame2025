acaf1fe8cb6912f744464864143b11870e2ef230
Anatoli Scameroni <anatoli.scameroni@avanade.com>
2026-05-05 17:07:58 +0200

    Update plugin: Version 1.2

--- Plugin.sc_ProductDetails/BusinessLogicPlugins/OnCreateUpdateOrderAcquistoCheckExistKeyProductDetailsLogic.cs
+++ Plugin.sc_ProductDetails/BusinessLogicPlugins/OnCreateUpdateOrderAcquistoCheckExistKeyProductDetailsLogic.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Plugin.sc_ProductDetails.Entities;
 using Plugin.sc_ProductDetails.Helper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,15 +24,77 @@ namespace Plugin.sc_ProductDetails.BusinessLogicPlugins
             tracingService?.Trace("End Plugin OnCreateUpdateOrderAcquistoCheckExistKeyProductDetails");
         }
 
-        public void ExecuteOrderAcquistoCreate(IOrganizationService service, Entity target, ITracingService tracingService)
+        public void ExecuteOrderAcquistoCreate(IOrganizationService service, Entity target, ITracingService trace)
         {
             AcquistoHelper _acquistoHelper = new AcquistoHelper();
             KeyProdottoHelper _keyProductHelper = new KeyProdottoHelper();
             ProdottoDigitaleHelper _prodottoDigitaleHelper = new ProdottoDigitaleHelper();
-            Entity entityUpdate = new Entity("acn_ordineacquisto");
+            Entity entityUpdate = new Entity(OrderAcquisto.LogicalName);
             entityUpdate.Id = target.Id;
             Guid acquistoIdRetrive = Guid.Empty;
-            Guid videogameIdRetrive = Guid.Empty;
+            Guid pdIdRetrive = Guid.Empty;
+             
+            if (!target.TryGetAttributeValue(OrderAcquisto.ProdottoDigitaleId, out EntityReference prodottodigitaleTo))
+            {
+                trace?.Trace($"ProdottoDigitaleTO is null: {prodottodigitaleTo}");
+                return;
+            }
+            //  get ProdottoDigitaleTO
+            Entity getPDToTo = service.Retrieve(prodottodigitaleTo.LogicalName, prodottodigitaleTo.Id, new ColumnSet(true));
+
+            int? typePiattaforma = ((OptionSetValue)getPDToTo.Attributes[ProdottoDigitale.TypePiattaforma]).Value;
+            // Key Game di PD Based
+            List<Entity> keyProductArray = _keyProductHelper.ExistKeyProduct(service, prodottodigitaleTo, 126400000, typePiattaforma); // Disponibile;
+            if (keyProductArray.Count == 0)
+            {
+                trace?.Trace($"Le chiavi non sono disponibile: {keyProductArray.Count}");
+                return;
+            }
+
+            if (!target.TryGetAttributeValue(OrderAcquisto.AcquistoId, out EntityReference acquistoTo))
+            {
+                trace?.Trace($"acquistoTo is null: {acquistoTo}");
+
+                Guid accountId = getPDToTo.GetAttributeValue<EntityReference>(ProdottoDigitale.AccountCliente)?.Id ?? Guid.Empty;
+                pdIdRetrive = getPDToTo.GetAttributeValue<Guid>(ProdottoDigitale.ProdottoDigitaleId);
+                List<Entity> acquistiInattesa = _acquistoHelper.GetAcquistoInAttesa(service, accountId);
+                if (acquistiInattesa.Count > 0)
+                {
+                    Guid acquistoId = acquistiInattesa[0].GetAttributeValue<Guid>(Acquisto.AcquistoId);
+                    acquistoIdRetrive = acquistoId;
+                }
+                else
+                {
+                    int quantitaAcquisto = _acquistoHelper.GetAcquisto(service, target).Entities.Count + 1;
+
+                    Entity nuovoAcquisto = new Entity(Acquisto.LogicalName);
+                    nuovoAcquisto[Acquisto.Name] = "acquisto" + quantitaAcquisto.ToString() + DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+                    nuovoAcquisto[Acquisto.AccountId] = new EntityReference("account", accountId); // Associa l'account
+                    nuovoAcquisto[Acquisto.KestatusAcquisto] = new OptionSetValue(126400001); // Stato "In Attesa" (Assumendo che il valore sia 100000000)
+                    nuovoAcquisto[Acquisto.Code] = GeneraCodiceAcquisto();
+                    Guid acquistoId = service.Create(nuovoAcquisto);
+                    acquistoIdRetrive = acquistoId;
+                    trace?.Trace($"Nuovo Acquisto creato: {acquistoTo}");
+                }
+
+                entityUpdate[OrderAcquisto.AcquistoId] = new EntityReference(Acquisto.LogicalName, acquistoIdRetrive);
+
+            }
+            trace?.Trace($"AssignTo {acquistoTo}");
+
+            _keyProductHelper.UpdateKeyProduct(service, keyProductArray[0].Id, 126400004);// Temporaneamente 
+
+            entityUpdate[OrderAcquisto.KeyProdottoDigitale] = keyProductArray[0].GetAttributeValue<string>(KeyProdotto.KeyDigitale);// Padre key
+
+            service.Update(entityUpdate);
+        }
+
+        private string GeneraCodiceAcquisto(int lunghezza = 6)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return "ACQ-" + new string(Enumerable.Repeat(chars, 6)
+                                              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
-}
+} 
